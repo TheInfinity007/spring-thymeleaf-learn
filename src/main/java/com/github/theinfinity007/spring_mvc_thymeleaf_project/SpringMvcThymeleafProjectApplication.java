1afd9a19f49db64e2715c0792879ca1df5b03c43
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-27 01:21:02 +0530

    init project

---
+++ src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/SpringMvcThymeleafProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.theinfinity007.spring_mvc_thymeleaf_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymeleafProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/SpringMvcThymeleafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.theinfinity007.spring_mvc_thymeleaf_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
