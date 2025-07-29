cc14ceeabeea6e214c4cd08f784f7adedf835ab7
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-29 23:05:51 +0530

    add the process form page

--- src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/controller/HelloWorldController.java
+++ src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.github.theinfinity007.spring_mvc_thymeleaf_project.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,26 @@ public class HelloWorldController {
     // Controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld-process-form";
+    }
+
+    // Controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read paramerter from form
+        String name = request.getParameter("studentName");
+
+        // convert to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + name;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld-process-form";
     }
 }
