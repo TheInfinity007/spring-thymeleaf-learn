62072e9449c801c194a975063ef3ddfc39dea489
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-07-29 23:17:45 +0530

    Spring MVC binding request param

--- src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/controller/HelloWorldController.java
+++ src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -33,6 +34,22 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + name;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld-process-form";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String sName, Model model){
+
+
+        // convert to all caps
+        sName = sName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend from v3! " + sName;
 
         // add message to the model
         model.addAttribute("message", result);
