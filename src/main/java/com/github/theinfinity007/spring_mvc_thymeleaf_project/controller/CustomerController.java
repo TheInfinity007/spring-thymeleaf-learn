bc38974dd6f94152c3510a5ad75d5e8a53a1a05e
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-08-07 23:21:05 +0530

    Trim the input data using initbinder and stringtrimmereditor

--- src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/controller/CustomerController.java
+++ src/main/java/com/github/theinfinity007/spring_mvc_thymeleaf_project/controller/CustomerController.java
@@ -3,15 +3,29 @@ package com.github.theinfinity007.spring_mvc_thymeleaf_project.controller;
 import com.github.theinfinity007.spring_mvc_thymeleaf_project.model.Customer;
 import com.github.theinfinity007.spring_mvc_thymeleaf_project.model.Student;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // initbinder to convert trim input strings
+    // remove the leading and trailing whitespace
+    // resolve issue for the validation
+
+    // It will be called for every request coming to the controller
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/showCustomerForm")
     public String showForm(Model model) {
